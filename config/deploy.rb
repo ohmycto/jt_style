056213cf44406e57c605b29aadbd18b93978f426
Denis I. Lukyanov <d.lukyanov@secoint.ru>
2012-03-04 15:18:04 +0400

    Deploy script changed

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,20 +33,22 @@ task :copy_database_config, roles => :app do
   run "cp #{db_config} #{release_path}/config/database.yml"
   run "ln -s #{shared_path}/assets #{release_path}/public/assets"
   run ["cd #{deploy_to}/current",
-       "#{bundler} install --path ~/.gem",
+       "rvm use ree-1.8.7-2011.03 do bundle install --path ../../shared/gems",
        "#{bundler} exec #{rake} jt_style_theme:install"].join(" && ")
 end
 
 set :unicorn_conf, "/etc/unicorn/jt-style.filippovaw.rb"
 set :unicorn_pid, "/var/run/unicorn/jt-style.filippovaw.pid"
 
-set :unicorn_start_cmd, "(cd #{deploy_to}/current; rvm use ree-1.8.7 do bundle exec unicorn_rails -Dc #{unicorn_conf})"
+set :unicorn_start_cmd, "(cd #{deploy_to}/current; rvm use ree-1.8.7-2011.03 do bundle exec unicorn_rails -Dc #{unicorn_conf})"
 
 # - for unicorn - #
 namespace :deploy do
   desc "Start application"
   task :start, :roles => :app do
-    run unicorn_start_cmd
+    run ["cd #{deploy_to}/current",
+        "rvm use ree-1.8.7-2011.03 do bundle install --path ../../shared/gems",
+        unicorn_start_cmd].join(" && ")
   end
 
   desc "Stop application"
